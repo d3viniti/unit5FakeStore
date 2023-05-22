babf066335bc9f09bf6b331fa3a38006d7ccd54e
d3viniti <119727268+d3viniti@users.noreply.github.com>
2023-05-21 21:02:27 -0700

    added basic css styling to ItemCard

--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -4,8 +4,8 @@ import './ItemCard.css'
 function ItemCard({item}) {
   return (
     <div className='item-card'>
-        <img src={item.image} />
-        <p>{item.title}</p>
+        <img src={item?.image} />
+        <p>{item?.title}</p>
         <a href="#">See Details</a>
     </div>
   )
