5f34c137189a4f4fa691cfe82d9b90267cd97e61
d3viniti <119727268+d3viniti@users.noreply.github.com>
2023-06-04 11:15:40 -0700

    renamed to Product Card and props

--- src/contexts/CartContext/CartContext.jsx
+++ src/contexts/CartContext/CartContext.jsx
@@ -7,8 +7,25 @@ export default function CartContextProvider(props){
     //create state to hold items in cart
     const [cartItems, setCartItems] = useState([])
 
+
+    //function to add item to card
+    const addItem = (itemToAdd) =>{
+        //when setting up a func that's connected to something
+        //verify its connection before proceeding
+        console.log('adding', itemToAdd)
+        //add this item to state
+        let newItems = [...cartItems, itemToAdd]
+        console.log(newItems)
+        //this console.log to be sure we can add to the empty array
+        //now, store in state
+        setCartItems(newItems)
+    }
+    //because above func will be the onclick in itemCard, must add
+    //it as a value in the CartContext.Provider tags
+
     return(
-        <CartContext.Provider value={{cartItems}}>
+        
+        <CartContext.Provider value={{cartItems, addItem}}>
             {props.children}
         </CartContext.Provider>
     )
